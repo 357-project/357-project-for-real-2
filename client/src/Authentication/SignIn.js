49d9e5d224a6694a2c1fcdd034fd4521acda65c5
Andre <psnmeee@gmail.com>
2022-04-02 12:16:23 -0400

    added auth pages

--- client/src/Authentication/SignIn.js
+++ client/src/Authentication/SignIn.js
@@ -1,38 +1,46 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-import 'bootstrap/dist/css/bootstrap.css';
-import '../App.css'
-import {Link} from "react-router-dom";
+import * as React from 'react';
+import LockOpenTwoToneIcon from '@mui/icons-material/LockOpenTwoTone';
+import { Container, Typography, Box, Grid, Link, Checkbox, FormControlLabel, TextField, CssBaseline, Button, Avatar } from '@mui/material';
 
-const SignIn = ()=> {
+
+
+//handling the login form
+function SignIn() {
     return (
-        <div className="App">
-            <h2>Sign In</h2>
-            <Form className="form">
-                <FormGroup>
-                    <Label for="exampleEmail">Username</Label>
-                    <Input
-                        type="email"
-                        name="email"
-                        id="exampleEmail"
-                        placeholder="example@example.com"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input
-                        type="password"
-                        name="password"
-                        id="examplePassword"
-                        placeholder="********"
-                    />
-                </FormGroup>
-                <Link to='/MazeGame'>
-                    <Button className='button'>Submit</Button>
-                </Link>
-            </Form>
-        </div>
+        <>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+                    <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+                        <LockOpenTwoToneIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box sx={{ mt: 1 }}>
+                        {/* Getting the email and password from user */}
+                        <TextField required fullWidth id="email" label="Email Address" name="email" autoComplete="email" />
+                        <TextField required fullWidth name="password" label="Password" type="password" id="password" autoComplete="new-password"/>
+                        <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+                        <Button type="submit" fullWidth variant="contained" href="/MazeGame" sx={{ mt: 3, mb: 2 }}>
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="/Signup" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </>
     );
 }
 
--- client/src/Authentication/SignUp.js
+++ client/src/Authentication/SignUp.js
@@ -1,48 +1,58 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-import 'bootstrap/dist/css/bootstrap.css';
-import '../App.css'
-import {Link} from "react-router-dom";
+import * as React from 'react';
+import LockOpenTwoToneIcon from '@mui/icons-material/LockOpenTwoTone';
+import { Container, Typography, Box, Grid, Link, TextField, CssBaseline, Button, Avatar} from '@mui/material'
 
-const SignUp = ()=> {
+
+function SignUp() {
     return (
-        <div className="App">
-            <h2>Sign Up</h2>
-            <Form className="form">
-                <FormGroup>
-                    <Label for="exampleEmail">Username</Label>
-                    <Input
-                        type="email"
-                        name="email"
-                        id="exampleEmail"
-                        placeholder="example@example.com"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input
-                        type="password"
-                        name="password"
-                        id="examplePassword"
-                        placeholder="********"
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Verify Password</Label>
-                    <Input
-                        type="password"
-                        name="password"
-                        id="examplePassword"
-                        placeholder="********"
-                    />
-                </FormGroup>
-                <Link to='/SignIn'>
-                    <Button className='button' path='/Home'>Submit</Button>
-                </Link>
 
-            </Form>
-        </div>
+        <>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+                    <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+                        <LockOpenTwoToneIcon />
+                    </Avatar>
+                    {/* Displays the sign up form */}
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            {/* Taking input from user */}
+                            <Grid item xs={12} sm={6}>
+                                <TextField autoComplete="given-name" name="firstName" required fullWidth id="firstName" label="First Name" autoFocus />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField required fullWidth id="lastName" label="Last Name" name="lastName" autoComplete="family-name" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField required fullWidth id="Phone Number" label="Phone Number" name="PhoneNumber" autoComplete="phone-number"/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField required fullWidth id="email" label="Email Address" name="email" autoComplete="email" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField required fullWidth name="password" label="Password" type="password" id="password" autoComplete="new-password"/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField required fullWidth name="confirmPassword" label="Confirm Password" type="password" id="Confirmpassword" autoComplete="new-password"/>
+                            </Grid>
+                        </Grid>
+                        <Button type="submit" fullWidth variant="contained" href="/signin" sx={{ mt: 3, mb: 2 }}>
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="/signin" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </>
     );
 }
 
